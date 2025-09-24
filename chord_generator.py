f05c150d6d1e00bb341ed34c181e6c86005aeb5f
marcocmaj7 <chitarristamrc@yahoo.it>
2025-09-24 20:46:12 +0200

    dd

--- chord_generator.py
+++ chord_generator.py
@@ -484,11 +484,10 @@ class ColorTreeDisplayApp:
         
         # Calcola la larghezza in base al livello
         if sound_cell.level == 12:
-            # Per il livello 12, calcola la larghezza per occupare tutto lo spazio
-            # Simula la larghezza che avrebbero 12 caselle normali
-            cell_width = 130 * 12  # 12 caselle da 130px ciascuna
+            # Per il livello 12, mantiene la larghezza originale
+            cell_width = 130 * 12  # 12 caselle da 130px ciascuna (dimensione originale)
         else:
-            cell_width = 140  # Larghezza aumentata di 25px (130 + 25 = 155) per tutti gli altri livelli
+            cell_width = 160  # Larghezza aumentata di 20px (140 + 20 = 160) per tutti gli altri livelli
         
         # Frame principale della sound cell - dimensioni bilanciate e centrate
         main_cell = tk.Frame(parent, bg=bg_color, relief='raised', bd=1, width=cell_width, height=70)
